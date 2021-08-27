45b6407a3cf880dc1a3be799d3a96cc6a3556a82
Imekaferdy <Imekaferdy20@gmail.com>
2021-08-27 02:16:59 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+/**
+ * main - puts char
+ *
+ * return: always 0 (Success)
+ */
+int main(voidi)
 {
 char app[51];
 strcpy(app, "Programming is like building a multilingual puzzle");
 puts(app);
-return (0);
+    return (0);
 }
 

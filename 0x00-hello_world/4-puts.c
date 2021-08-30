bf284f5a5b28dca0430f98e26752594dfa35b81f
Imekaferdy <Imekaferdy20@gmail.com>
2021-08-29 17:30:19 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-char app[51];
-strcpy(app, "\"Programming is like building a multilingual puzzle");
-puts(app);
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 

6d982183806ceb59a7478bdb870c114493bfa4a9
Imekaferdy <Imekaferdy20@gmail.com>
2021-08-30 17:23:00 -0700

    if_else_statement

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main: numbers from 0 to 10
+ * main - numbers from 0 to 10
  *
  * Description: prints out all the numbers from 0-10
- *
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)

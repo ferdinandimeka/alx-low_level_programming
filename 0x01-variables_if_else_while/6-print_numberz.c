a9e4253444af0d2445dce85b29571195056f228f
Imekaferdy <Imekaferdy20@gmail.com>
2021-08-30 17:58:49 -0700

    if_else_statement

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - all single digits 
+ * main - all single digits
  * description: Prints from 0 to 9
  * Return: 0 (Success)
  */
@@ -10,7 +10,9 @@ int main(void)
 	int num;
 
 	for (num = 0; num <= 10; num++)
+	{
 	putchar(num);
+	}
 	putchar('\n');
 	return (0);
 }

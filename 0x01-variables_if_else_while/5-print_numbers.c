e46ecd6d2a506b4bb9ef92696479889da81b2f84
Imekaferdy <Imekaferdy20@gmail.com>
2021-08-30 17:07:09 -0700

    if_else_statement

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main: single digit numbers from 0 to 10
+ *
+ * Description: prints out all the numbers from 0-10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	printf("%d", x);
+	printf("\n");
+	return (0);
+}

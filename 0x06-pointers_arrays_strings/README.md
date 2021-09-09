10ed3c4c46d6db47b7a7cf9d86965cc0c2574daf
Imekaferdy <Imekaferdy20@gmail.com>
2021-09-09 16:51:33 -0700

    C concat

--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,3 @@
 1. 0-strcat concatenates two strings.
+2. 1-strncat.c concatenates two strings.
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 /* Header files */
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif

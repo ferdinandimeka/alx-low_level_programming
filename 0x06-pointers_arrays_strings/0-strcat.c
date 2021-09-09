394924fa377922e504b24c54177d29225bf8302e
Imekaferdy <Imekaferdy20@gmail.com>
2021-09-09 08:00:35 -0700

    C concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: first pointer
+ * @src: second pointer
+ *
+ * Return: string value
+ */
+char *_strcat(char *dest, char *src)
+{
+	int ch; int sh;
+
+	ch = 0;
+	sh = 0;
+	while (dest[ch] != 0)
+	{
+	ch++;
+	}
+	while (src[sh] != 0)
+	{
+	dest[ch + sh] = src[sh];
+	sh++;
+	}
+	
+	dest[ch + sh] = 0;
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Header files */
+
+char *_strcat(char *dest, char *src);
+
+#endif

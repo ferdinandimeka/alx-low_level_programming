eaebe86d16dc5236b630951717595b310f12f64e
Imekaferdy <Imekaferdy20@gmail.com>
2021-09-24 08:24:36 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,14 +14,20 @@ void *_calloc(unsigned int nmemb, unsigned int size)
         unsigned int i;
 
         if (nmemb == 0 || size == 0)
+	{
         return (NULL);
+	}
 
         arr = malloc(nmemb * size);
         if (arr == NULL)
+	{
         return (NULL);
+	}
 
         for (i = 0; i < (nmemb * size); i++)
+	{
         arr[i] = 0;
+	}
 
         return (arr);
 }

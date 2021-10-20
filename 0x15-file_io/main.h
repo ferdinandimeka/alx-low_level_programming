a689577c151d0de3aad857eda0b6257f27e96dc2
Imekaferdy <Imekaferdy20@gmail.com>
2021-10-20 10:16:41 -0700

    file_io

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define ERROR97  "Usage: cp file_from file_to\n"
+#define ERROR98  "Error: Can't read from file %s\n"
+#define ERROR99  "Error: Can't write to %s\n"
+#define ERROR100 "Error: Can't close fd %d\n"
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */

6ab767d21d340c260cce6a9de697cb53ea366c41
Imekaferdy <Imekaferdy20@gmail.com>
2021-10-20 11:01:34 -0700

    file_io

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: the number of arguments.
+ * @argv: arguments.
+ * Description:  description
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
+}

9aa60858f3405510f6a1bb693041ca0d3b886ed9
imekaferdy <imekaferdy20@gmail.com>
2022-06-03 00:24:26 +0100

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,35 +11,35 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	i = 48;
-	j = 49;
-	while  ((i < 57) && (j < 58))
+i = 48;
+j = 49;
+while  ((i < 57) && (j < 58))
+{
+	putchar(i);
+	putchar(j);
+	if ((i == 56) && (j == 57))
 	{
-		putchar(i);
-		putchar(j);
-		if ((i == 56) && (j == 57))
+		putchar('\n');
+		i++;
+		j++;
+	}
+	else
+	{
+		putchar(44);
+		putchar(32);
+		if (j < 57)
 		{
-			putchar('\n');
-			i++;
 			j++;
 		}
 		else
 		{
-			putchar(44);
-			putchar(32);
-			if (j < 57)
-			{
-				j++;
-			}
-			else
-			{
-				i++;
-				j = 1 + i;
-			}
+			i++;
+			j = 1 + i;
 		}
 	}
-	return (0);
+}
+return (0);
 }

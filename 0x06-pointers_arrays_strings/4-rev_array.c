a498f3d07f9f3b075c60928f810339cb1965562b
imekaferdy <imekaferdy20@gmail.com>
2022-06-03 22:47:31 +0100

    update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,31 +1,19 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the value of two integers
- * @a: pointer a
- * @b: pointer b
- */
-void swap_int(int *a, int *b)
-{
-	*a = *b + *a;
-	*b = *a - *b;
-	*a = *a - *b;
-}
-
-/**
- * reverse_array - reverses the content of an array of integers
- * @a: an array of integers
- * @n: the number of element to swap
- *
- * Return: string value.
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
  */
 void reverse_array(int *a, int n)
 {
-	int rev = 0;
+	int i;
+	int temp;
 
-	while (rev < n / 2)
+	for (i = 0; i < n--; i++)
 	{
-	swap_int(a + rev, a + (n - rev - 1));
-	rev++;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 	}
 }

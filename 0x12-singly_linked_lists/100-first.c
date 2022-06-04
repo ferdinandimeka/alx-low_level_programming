8b75cfabc90666d8b44a065ea6dae8164f65a9a7
imekaferdy <imekaferdy20@gmail.com>
2022-06-05 00:31:05 +0100

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 void print(void)__attribute__((constructor));
 /**
- * main - Entry point
+ * print - prints a string
  *
  * Return: string value
  */
 void print(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back\n");
 }

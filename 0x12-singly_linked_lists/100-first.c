a3b226804f0861a18aa892988ebc0c3f61f1ae8c
imekaferdy <imekaferdy20@gmail.com>
2022-06-05 00:33:11 +0100

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@ void print(void)__attribute__((constructor));
 void print(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back\n");
+	printf("I bore my house upon my back!\n");
 }

3ce10840a4ca4b06e297b561fbf7143d116e8183
ferdinandimeka <117474473+ferdinandimeka@users.noreply.github.com>
2023-05-09 19:10:58 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,8 +15,8 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-            return (i);
+            	return (i);
 		}
 	}
-    return (-1);
+    	return (-1);
 }

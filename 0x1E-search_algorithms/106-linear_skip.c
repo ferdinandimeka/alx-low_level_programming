d3ee49590bce762219f28bf66ce1e7d44c909dce
ferdinandimeka <117474473+ferdinandimeka@users.noreply.github.com>
2023-05-09 19:30:48 +0100

    Update 106-linear_skip.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,72 +1,45 @@
 #include "search_algos.h"
 
-skiplist_t *recurse_normal(skiplist_t *probe, skiplist_t *stop, int value);
-skiplist_t *recurse_express(skiplist_t *probe, int value);
-skiplist_t *find_list_end(skiplist_t *probe);
-
 /**
- * linear_skip - perform search with skip list
- * @list: list to search
- * @value: search value
+ * linear_skip - searches for a value in a skip list
  *
- * Return: matching node; NULL if not found
+ * @list: input list
+ * @value: value to search in
+ * Return: index of the number
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *zone = NULL;
+	skiplist_t *go;
 
 	if (list == NULL)
 		return (NULL);
 
-	zone = recurse_express(list, value);
+	go = list;
 
-	if (zone->n == value)
-		return (zone);
-	else
-		return (recurse_normal(zone, zone->express, value));
-}
-/**
- * recurse_express - search express list
- * @probe: search pointer
- * @value: search value
- *
- * Return: pointer to match or match range; NULL if not in range
- */
-skiplist_t *recurse_express(skiplist_t *probe, int value)
-{
-	skiplist_t *last = NULL;
+	do {
+		list = go;
+		go = go->express;
+		printf("Value checked at index ");
+		printf("[%d] = [%d]\n", (int)go->index, go->n);
+	} while (go->express && go->n < value);
 
-	if (probe->express == NULL)
+	if (go->express == NULL)
 	{
-		last = find_list_end(probe);
-		printf("Value found between indexes [%lu] and [%lu]\n",
-probe->index, last->index);
-		return (probe);
+		list = go;
+		while (go->next)
+			go = go->next;
 	}
 
-	printf("Value checked at index [%lu] = [%d]\n",
-probe->express->index, probe->express->n);
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
 
-	if (probe->express->n >= value)
+	while (list != go->next)
 	{
-		printf("Value found between indexes [%lu] and [%lu]\n",
-probe->index, probe->express->index);
-		return (probe);
+		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
 	}
-	else
-		return (recurse_express(probe->express, value));
-}
-/**
- * recurse_normal - search normal list
- * @probe: search pointer
- * @stop: endpoint of subsearch; either express node or NULL
- * @value: search value
- *
- * Return: pointer to match; NULL if not found
- */
-skiplist_t *recurse_normal(skiplist_t *probe, skiplist_t *stop, int value)
-{
-	if (probe == stop)
-	{
-		if (stop != NULL && stop->n == value)
 
+	return (NULL);
+}
